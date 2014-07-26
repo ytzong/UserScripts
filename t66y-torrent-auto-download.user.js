8f18d8af69b603aa652048c6a51b8b4db6dd750a
ytzong <ytzong7@gmail.com>
2014-07-26 20:19:08 +0800

    add website

--- t66y-torrent-auto-download.user.js
+++ t66y-torrent-auto-download.user.js
@@ -4,13 +4,14 @@
 // @version    0.1
 // @description  FFH6 Auto Download
 // @match      http://www.ffh6.com/bt*
+// @match      http://www.yyyy1.info/bt*
 // @match      http://www.rmdown.com/link.php*
 // @copyright  2014+, ytzong
 // ==/UserScript==
 
 function ytDownloadTorrent(){
     var strDomain = window.location.hostname;
-    if (strDomain == 'www.ffh6.com')
+    if (strDomain == 'www.ffh6.com' || strDomain == 'www.yyyy1.info')
     	document.getElementsByClassName('button')[0].click();
     if (strDomain == 'www.rmdown.com')
         document.forms[0].submit.click();
