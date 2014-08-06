c136fc0071beb213bc995f8b478fe3467eb6b42c
ytzong <ytzong7@gmail.com>
2014-08-06 19:02:58 +0800

    支持第三方网站登录

--- qq-auto-login.user.js
+++ qq-auto-login.user.js
@@ -4,13 +4,26 @@
 // @version    0.1
 // @description  QQ Auto Login
 // @match      http://ui.ptlogin2.qq.com/cgi-bin/login*
+// @match      http://xui.ptlogin2.qq.com/cgi-bin/xlogin*
 // @copyright  2014+, ytzong
 // ==/UserScript==
 
-document.getElementById('u').value = ''; //后面引号里输入你的QQ号码
-document.getElementById('p').value = ''; //后面引号里输入你的密码
+var strDomain = window.location.hostname;
 
-function ytFunc(){
+if (strDomain == 'xui.ptlogin2.qq.com') {
+	document.getElementById('switcher_plogin').click();
+    ytFunc1();
+}
+
+if (strDomain == 'ui.ptlogin2.qq.com') {
+    ytFunc1();
+    window.setTimeout(ytFunc2, 1000);
+}
+
+function ytFunc1(){
+    document.getElementById('u').value = ''; //后面引号里输入你的QQ号码
+    document.getElementById('p').value = ''; //后面引号里输入你的密码
+}
+function ytFunc2(){
     document.getElementById('login_button').click();
 }
-window.setTimeout(ytFunc, 1000);
