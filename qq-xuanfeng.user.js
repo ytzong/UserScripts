8e58e1d386071b8fee209b6cdfda2c8866a1aebf
ytzong <ytzong7@gmail.com>
2014-08-06 18:21:04 +0800

    qq xuanfeng

---
+++ qq-xuanfeng.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name       QQ Xuanfeng
+// @namespace  QQ Xuanfeng
+// @version    0.1
+// @description  QQ Xuanfeng
+// @match      http://lixian.qq.com/login.html
+// @copyright  2014+, ytzong
+// ==/UserScript==
+
+document.getElementById('login_btn').click();
