d01294505c75309b2eb3ece3c1a5038d1e09224e
ytzong <ytzong7@gmail.com>
2014-08-13 16:15:37 +0800

    反电信劫持

---
+++ fuck-gd10000.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       Fuck GD10000
+// @namespace  http://twitter.com/ytzong/
+// @version    0.1
+// @description  enter something useful
+// @match      *
+// @copyright  2014+, ytzong
+// ==/UserScript==
+
+if(document.getElementById("contenid")) {
+	window.location.reload();
+};
