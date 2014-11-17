9c964a7c8f186babc611a9287509151968f9d3e3
ytzong <ytzong7@gmail.com>
2014-11-17 11:19:26 +0800

    add mp4ba

--- douban-movie-download-link.user.js
+++ douban-movie-download-link.user.js
@@ -2,7 +2,9 @@
 // @name           Douban Movie Download Search
 // @namespace      Douban Movie Download Search
 // @description    Douban Movie Download Search
-// @include        http://movie.douban.com/subject/*
+// @match        http://movie.douban.com/subject/*
+// @version    0.2
+// @copyright  2012+, ytzong
 // ==/UserScript==
 
 function addJQuery(callback) {
@@ -19,7 +21,7 @@ function addJQuery(callback) {
 function main() {
     try {
     	var movieTitle = jQuery('h1 span:eq(0)').text();
-		jQuery('#info').append('<div><span class="pl">下载链接:</span> <a href="http://simplecd.me/search/entry/?query=' + movieTitle + '" target="_balnk">SimpleCD</a> / <a href="http://btmee.net/search/?q=' + movieTitle + '" target="_balnk">BTmee</a><br><span class="pl">字幕链接:</span> <a href="http://shooter.cn/search/' + movieTitle + '" target="_balnk">Shooter</a><div>');
+		jQuery('#info').append('<div><span class="pl">下载链接:</span> <a href="http://simplecd.me/search/entry/?query=' + movieTitle + '" target="_balnk">SimpleCD</a> / <a href="http://btmee.net/search/?q=' + movieTitle + '" target="_balnk">BTmee</a> / <a href="http://www.mp4ba.com/search.php?keyword=' + movieTitle + '" target="_balnk">MP4BA</a><br><span class="pl">字幕链接:</span> <a href="http://shooter.cn/search/' + movieTitle + '" target="_balnk">Shooter</a><div>');
     }
     catch (e) { }
 }
