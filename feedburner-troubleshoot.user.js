5fae4c262a337bc9ddab88da9eafbec6af88b43d
ytzong <ytzong7@gmail.com>
2015-01-12 14:40:59 +0800

    bug fixes

--- feedburner-troubleshoot.user.js
+++ feedburner-troubleshoot.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name       feedburner-troubleshoot
 // @namespace  http://twitter.com/ytzong
-// @version    0.1
+// @version    0.2
 // @description  feedburner-troubleshoot
 // @match      https://feedburner.google.com/fb/a/troubleshoot?id=*
 // @copyright  2015+, ytzong
 // ==/UserScript==
 
 function YTFunc(){
+	if (document.getElementsByClassName('confirm message').length == 0)
         document.forms[3].submit();
 }
 window.setTimeout(YTFunc, 2000);
