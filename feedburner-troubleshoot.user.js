07b388d173c2350714d80b4624860b455a6f5033
ytzong <ytzong7@gmail.com>
2015-01-12 14:45:55 +0800

    auto redirect

--- feedburner-troubleshoot.user.js
+++ feedburner-troubleshoot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       feedburner-troubleshoot
 // @namespace  http://twitter.com/ytzong
-// @version    0.2
+// @version    0.3
 // @description  feedburner-troubleshoot
 // @match      https://feedburner.google.com/fb/a/troubleshoot?id=*
 // @copyright  2015+, ytzong
@@ -10,5 +10,7 @@
 function YTFunc(){
 	if (document.getElementsByClassName('confirm message').length == 0)
         document.forms[3].submit();
+    else
+    	window.location.href = document.querySelector('#thingActions a').getAttribute('href');
 }
 window.setTimeout(YTFunc, 2000);
