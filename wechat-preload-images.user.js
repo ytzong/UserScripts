1fce056fd4409e7e46abc816d597184e8c49cb0b
ytzong <ytzong7@gmail.com>
2015-03-02 22:01:25 +0800

    取消微信文章 lazy load

---
+++ wechat-preload-images.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         WeChat Preload Images
+// @namespace    http://twitter.com/ytzong
+// @version      0.1
+// @description  WeChat Preload Images
+// @author       ytzong
+// @match        http://mp.weixin.qq.com/*
+// @match        https://mp.weixin.qq.com/*
+// @grant        none
+// ==/UserScript==
+
+
+function addJQuery(callback) {
+    var script = document.createElement("script");
+    script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+    script.addEventListener('load', function () {
+        var script = document.createElement("script");
+        script.textContent = "(" + callback.toString() + ")();";
+        document.body.appendChild(script);
+    }, false);
+    document.body.appendChild(script);
+}
+function main() {
+    $('img').each(function(){
+        var dataSrc = $(this).attr('data-src');
+        if (dataSrc)
+            $(this).attr('src', dataSrc);
+    })
+}
+addJQuery(window.setTimeout(main, 2000));
