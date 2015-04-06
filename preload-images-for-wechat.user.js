8fbbebf5790ac28f416e1fa96f55733c6c293285
ytzong <ytzong7@gmail.com>
2015-04-06 23:57:56 +0800

    change jquery url

--- preload-images-for-wechat.user.js
+++ preload-images-for-wechat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Preload Images for WeChat
 // @namespace    http://twitter.com/ytzong
-// @version      0.1
+// @version      0.2
 // @description  Preload Images for WeChat
 // @author       ytzong
 // @match        http://mp.weixin.qq.com/*
@@ -12,7 +12,7 @@
 
 function addJQuery(callback) {
     var script = document.createElement("script");
-    script.setAttribute("src", "//code.jquery.com/jquery-latest.min.js");
+    script.setAttribute("src", "//code.jquery.com/jquery-2.1.3.min.js");
     script.addEventListener('load', function () {
         var script = document.createElement("script");
         script.textContent = "(" + callback.toString() + ")();";
