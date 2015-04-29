6878d733dcd54aec8e74fce49cbf1bd165acda5b
ytzong <ytzong7@gmail.com>
2015-04-30 01:51:19 +0800

    bug fixes

--- dmm-large-hoto.user.js
+++ dmm-large-hoto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DMM Large Photo
 // @namespace    http://twitter.com/ytzong
-// @version      0.5
+// @version      0.6
 // @description  DMM Large Photo
 // @author       ytzong
 // @match        http://www.dmm.co.jp/*
@@ -9,7 +9,7 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle('.d-item .list li {float:none !important; width:auto !important} #main-src .tmb:hover .img img{opacity:1 !important} #main-src[class*="digital"] .sample { top: 0!important;  left: 0!important;  margin-left: 0!important;}');
+GM_addStyle('.d-item #list li {float:none !important; width:auto !important} #main-src .tmb:hover .img img{opacity:1 !important} #main-src[class*="digital"] .sample { top: 0!important;  left: 0!important;  margin-left: 0!important;}');
 
 
 function YTFunc(){
@@ -19,7 +19,7 @@ function YTFunc(){
         $(this).attr('src', img);
         $(this).attr('style', 'max-width:none;max-height:none;margin-left:12px');
     })
-    $('.d-item .list a').each(function(){
+    $('.d-item #list a').each(function(){
         $(this).attr('target', '_blank');
     })
     
