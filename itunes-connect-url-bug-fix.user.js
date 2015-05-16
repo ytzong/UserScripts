ed1f0f619c8e1ef04bdcb0052bf1dc26901e17d2
ytzong <ytzong7@gmail.com>
2015-05-17 01:36:02 +0800

    + view in iTunes

--- itunes-connect-url-bug-fix.user.js
+++ itunes-connect-url-bug-fix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iTunes Connect URL Bug Fix
 // @namespace    http://twitter.com/ytzong
-// @version      0.1
+// @version      0.2
 // @description  iTunes Connect URL Bug Fix
 // @author       ytzong
 // @match        https://itunesconnect.apple.com/*
@@ -12,4 +12,7 @@ var url = window.location.href;
 
 if (url.substr(-2) == '#/') {
     window.location.href = url.substring(0, url.length -2)
+}    
+if (url.substr(-1) == '#') {
+    window.location.href = url.substring(0, url.length -1)
 }    
---
+++ view-in-itunes.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         View in iTunes
+// @namespace    http://twitter.com/ytzong
+// @version      0.1
+// @description  View in iTunes
+// @author       ytzong
+// @match        https://itunes.apple.com/*
+// @grant        none
+// ==/UserScript==
+
+$('#left-stack .view-in-itunes:eq(0)').click()
