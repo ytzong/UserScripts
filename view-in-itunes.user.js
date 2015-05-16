1ed2f8ce0ddb30714d41e28f385675dd623a27c8
ytzong <ytzong7@gmail.com>
2015-05-17 01:47:40 +0800

    bug fixes

--- view-in-itunes.user.js
+++ view-in-itunes.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         View in iTunes
 // @namespace    http://twitter.com/ytzong
-// @version      0.1
+// @version      0.2
 // @description  View in iTunes
 // @author       ytzong
 // @match        https://itunes.apple.com/*
 // @grant        none
 // ==/UserScript==
 
-$('#left-stack .view-in-itunes:eq(0)').click()
+$('#left-stack .view-in-itunes:eq(0)').click();
+
+window.setTimeout(window.close, 1000);
