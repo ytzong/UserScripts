d7e2cfb4d6ab61485c36efc0ea6cf0b1af4ad715
ytzong <ytzong7@gmail.com>
2015-10-08 00:25:30 +0800

    bug fixes

--- torrent-auto-download.user.js
+++ torrent-auto-download.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Torrent Auto Download
 // @namespace  http://twitter.com/ytzong
-// @version    0.7
+// @version    0.8
 // @description  Torrent Auto Download
 // @match      http://www.ffh6.com/*
 // @match      http://www.yyyy1.info/*
@@ -9,18 +9,20 @@
 // @match      http://www.jandown.com/*
 // @match      http://www.xahka.com/*
 // @match      http://www.222888.info/*
+// @match      http://www.bforu.info/*
+// @match      http://www.viidii.info/*
 // @copyright  2014+, ytzong
 // ==/UserScript==
 
 function ytDownloadTorrent(){
     var strDomain = window.location.hostname;
-    if (strDomain == 'www.ffh6.com' || strDomain == 'www.yyyy1.info')
+    if (strDomain == 'www.ffh6.com' || strDomain == 'www.yyyy1.info' || strDomain == 'www.222888.info' || strDomain == 'www.bforu.info')
     	document.getElementsByClassName('button')[0].click();
     if (strDomain == 'www.rmdown.com')
         document.forms[0].submit.click();
     if (strDomain == 'www.jandown.com' || strDomain == 'www.xahka.com')
         document.forms[0].submit();
-    if (strDomain == 'www.222888.info')
-    	document.getElementsByClassName('button')[0].click()
+    if (strDomain == 'www.viidii.info')
+        document.getElementsByClassName('bglink')[0].click();
 }
-window.setTimeout(ytDownloadTorrent, 2000);
+window.setTimeout(ytDownloadTorrent, 3000);
