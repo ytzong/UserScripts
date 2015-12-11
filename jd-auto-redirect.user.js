88fdb95149a631c66cd2fdf18596eea32886402c
ytzong <ytzong7@gmail.com>
2015-12-12 00:57:30 +0800

    +jd

---
+++ jd-auto-redirect.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         JD Auto Redirect
+// @namespace    http://twitter.com/ytzong
+// @version      0.2
+// @description  JD Auto Redirect
+// @author       ytzong
+// @match        http://re.jd.com/cps/item/*
+// @match        http://list.jd.com/*
+// @match        http://search.jd.com/*
+// @match        http://m.jd.com/sale/act/*
+// @grant        none
+// ==/UserScript==
+
+var domain = window.location.hostname;
+
+if (domain == 'm.jd.com') {    
+    $('#m_common_bottom_pc').trigger('click');
+}
+
+if (domain == 're.jd.com') {
+    window.location.href = $('.l_info_b a').attr('href');
+}
+
+if (domain == 'list.jd.com') {
+    var have = $('. instock  a');
+    if (!have.hasClass('selected')) 
+        window.location.href = have.attr('href');
+    
+    var isJD = $('.f-feature a:eq(0)');
+    if (!isJD.hasClass('selected')) 
+        window.location.href = isJD.attr('href');
+}
+
+if (domain == 'search.jd.com') {
+    var have = $('. instock  div:eq(0)');
+    if (!have.hasClass('selected')) 
+        window.location.href = have.children('a').attr('href');
+    
+    var isJD = $('#ware_type a:eq(0)');
+    if (!isJD.hasClass('selected')) 
+        window.location.href = isJD.attr('href');
+}
+
+
+
+
