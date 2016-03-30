b13c1837a07ccd1884323fd3b1583d0e6c64229e
ytzong <ytzong7@gmail.com>
2016-03-30 20:38:49 +0800

    bug fixes

--- douban-movie-download-link.user.js
+++ douban-movie-download-link.user.js
@@ -6,7 +6,7 @@
 // @match        http://book.douban.com/subject/*
 // @match        https://movie.douban.com/subject/*
 // @match        https://book.douban.com/subject/*
-// @version    1.0
+// @version    1.1
 // @copyright  2012+, ytzong
 // ==/UserScript==
 
@@ -18,7 +18,7 @@ if (document.domain == 'book.douban.com') {
 	downloadContent = '<div><span class="pl">下载链接:</span> <a href="https://www.google.com/search?sourceid=chrome&ie=UTF-8&q=' + movieTitle + '+azw3" target="_balnk">azw3</a> / <a href="https://www.google.com/search?sourceid=chrome&ie=UTF-8&q=' + movieTitle + '+mobi" target="_balnk">mobi</a> / <a href="https://www.google.com/search?sourceid=chrome&ie=UTF-8&q=' + movieTitle + '+epub" target="_balnk">epub</a> / <a href="http://readcolor.com/books/search?utf8=%E2%9C%93&s=' + movieTitle + '" target="_balnk">readfar</a><div>';
 }
 else {
-	downloadContent = '<div><span class="pl">下载链接:</span> <a href="http://dianying.fm/category/key_' + movieTitle + '" target="_balnk">电影FM</a> / <a href="http://btmee.net/search/?q=' + movieTitle + '" target="_balnk">BTmee</a> / <a href="http://www.btexe.com/search?kw=' + movieTitle + '" target="_balnk">BT磁力</a> / <a href="http://www.btbbt.cc/search-index-keyword-' + movieTitle + '.htm" target="_balnk">BTbbT</a> / <a href="https://www.google.com/search?sourceid=chrome&ie=UTF-8&q=' + movieTitle + '+1080p" target="_balnk">Google</a><br><span class="pl">字幕链接:</span> <a href="http://subhd.com/search/' + movieTitle + '" target="_balnk">SubHD</a> / <a href="http://www.zimuzu.tv/search?keyword=' + movieTitle + '" target="_balnk">Zimuzu</a><div>';
+	downloadContent = '<div><span class="pl">下载链接:</span> <a href="http://dianying.fm/category/key_' + movieTitle + '" target="_balnk">电影FM</a> / <a href="http://btmee.net/search/?q=' + movieTitle + '" target="_balnk">BTmee</a> / <a href="http://www.btexe.com/search?kw=' + movieTitle + '" target="_balnk">BT磁力</a> / <a href="http://www.btbbt.cc/search-index-keyword-' + movieTitle + '.htm" target="_balnk">BTbbT</a> / <a href="http://zhannei.baidu.com/cse/search?s=11180302771993919822&entry=1&q=' + movieTitle + '" target="_balnk">天天美剧</a> / <a href="https://www.google.com/search?sourceid=chrome&ie=UTF-8&q=' + movieTitle + '+1080p" target="_balnk">Google</a><br><span class="pl">字幕链接:</span> <a href="http://subhd.com/search/' + movieTitle + '" target="_balnk">SubHD</a> / <a href="http://www.zimuzu.tv/search?keyword=' + movieTitle + '" target="_balnk">字幕组</a><div>';
 }
 
 jQuery('#info').append(downloadContent);
