e008915013636484dac09f8546a77dee1139108b
ytzong <ytzong7@gmail.com>
2016-05-09 20:35:52 +0800

    bug fixes

--- jd-auto-redirect.user.js
+++ jd-auto-redirect.user.js
@@ -1,46 +1,42 @@
 // ==UserScript==
 // @name         JD Auto Redirect
 // @namespace    http://twitter.com/ytzong
-// @version      0.3
+// @version      0.4
 // @description  JD Auto Redirect
 // @author       ytzong
-// @match        http://re.jd.com/cps/item/*
-// @match        http://list.jd.com/*
-// @match        http://search.jd.com/*
-// @match        http://m.jd.com/sale/act/*
-// @grant        none
+// @include      http://re.jd.com/cps/item/*
+// @include      http://m.jd.com/sale/act/*
+// @include      http://item.m.jd.com/product/*
+// @run-at       document-idle
+// @grant        GM_addStyle
 // ==/UserScript==
 
 var domain = window.location.hostname;
 
-if (domain == 'm.jd.com') {    
-    $('#m_common_bottom_pc').trigger('click');
+if (domain == 'm.jd.com') {
+    var pathnames = location.pathname.split('/');
+    location.href = 'http://sale.jd.com/act/' + pathnames[pathnames.length - 1];
 }
-
+if (domain == 'item.m.jd.com') {
+    var pathnames = location.pathname.split('/');
+    location.href = 'http://item.jd.com/' + pathnames[pathnames.length - 1];
+}
 if (domain == 're.jd.com') {
     window.location.href = $('.gobuy a').attr('href');
 }
-
+/*
 if (domain == 'list.jd.com') {
     var have = $('. instock  a');
-    if (!have.hasClass('selected')) 
-        window.location.href = have.attr('href');
-    
+    if (!have.hasClass('selected')) window.location.href = have.attr('href');
     var isJD = $('.f-feature a:eq(0)');
-    if (!isJD.hasClass('selected')) 
-        window.location.href = isJD.attr('href');
+    if (!isJD.hasClass('selected')) window.location.href = isJD.attr('href');
 }
 
 if (domain == 'search.jd.com') {
     var have = $('. instock  div:eq(0)');
-    if (!have.hasClass('selected')) 
-        window.location.href = have.children('a').attr('href');
-    
+    if (!have.hasClass('selected')) window.location.href = have.children('a').attr('href');
     var isJD = $('#ware_type a:eq(0)');
-    if (!isJD.hasClass('selected')) 
-        window.location.href = isJD.attr('href');
+    if (!isJD.hasClass('selected')) window.location.href = isJD.attr('href');
 }
-
-
-
+*/
 
