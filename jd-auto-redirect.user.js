3f19a2f098b569a77ff8c2221cd02be07a81104b
ytzong <ytzong7@gmail.com>
2016-05-10 14:03:59 +0800

    bug fixes

--- jd-auto-redirect.user.js
+++ jd-auto-redirect.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         JD Auto Redirect
 // @namespace    http://twitter.com/ytzong
-// @version      0.4
+// @version      0.5
 // @description  JD Auto Redirect
 // @author       ytzong
-// @include      http://re.jd.com/cps/item/*
+// @include      http://re.jd.com/*
+// @include      http://re.m.jd.com/*
 // @include      http://m.jd.com/sale/act/*
 // @include      http://item.m.jd.com/product/*
 // @run-at       document-idle
@@ -17,26 +18,7 @@ if (domain == 'm.jd.com') {
     var pathnames = location.pathname.split('/');
     location.href = 'http://sale.jd.com/act/' + pathnames[pathnames.length - 1];
 }
-if (domain == 'item.m.jd.com') {
+if (domain == 'item.m.jd.com' || domain == 're.jd.com' || domain == 're.m.jd.com') {
     var pathnames = location.pathname.split('/');
     location.href = 'http://item.jd.com/' + pathnames[pathnames.length - 1];
 }
-if (domain == 're.jd.com') {
-    window.location.href = $('.gobuy a').attr('href');
-}
-/*
-if (domain == 'list.jd.com') {
-    var have = $('. instock  a');
-    if (!have.hasClass('selected')) window.location.href = have.attr('href');
-    var isJD = $('.f-feature a:eq(0)');
-    if (!isJD.hasClass('selected')) window.location.href = isJD.attr('href');
-}
-
-if (domain == 'search.jd.com') {
-    var have = $('. instock  div:eq(0)');
-    if (!have.hasClass('selected')) window.location.href = have.children('a').attr('href');
-    var isJD = $('#ware_type a:eq(0)');
-    if (!isJD.hasClass('selected')) window.location.href = isJD.attr('href');
-}
-*/
-
