1bed8c0c3d464dd9fe8b516cf41cb13ff5602b64
ytzong <ytzong7@gmail.com>
2016-07-13 17:50:40 +0800

    bug fixes

--- cocoapods-to-github.user.js
+++ cocoapods-to-github.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CocoaPods to GitHub
 // @namespace    http://twitter.com/ytzong
-// @version      0.1
+// @version      0.2
 // @description  CocoaPods to GitHub
 // @author       ytzong
 // @match        http://cocoapods.org/pods/*
@@ -9,4 +9,5 @@
 // @grant        none
 // ==/UserScript==
 
-window.location.href = $('.github-link a').attr('href');
+var gitlink = $('.github-link a');
+if (gitlink.length > 0) window.location.href = gitlink.attr('href');
