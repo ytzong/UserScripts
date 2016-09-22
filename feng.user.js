cc96fb19fce63bc95cf3e7e4dd20cb1823d89024
ytzong <ytzong7@gmail.com>
2016-09-22 22:28:17 +0800

    +feng

---
+++ feng.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Feng
+// @version      0.1
+// @author       ytzong
+// @description  Clean BBS
+// @include      http://bbs.feng.com/*
+// @include      https://bbs.feng.com/*
+// @copyright    2016+
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle('#bbs_top_news,#bbs_top_news + .wrap,.bm.bml.pbn,.wrap.fl_row,.wea_d_panel_980 {display: none !important;}');
