21440992439bcb0b7631198e061c126f9b955aab
ytzong <ytzong7@gmail.com>
2016-10-12 14:58:51 +0800

    bug fixes

--- jd-auto-redirect.user.js
+++ jd-auto-redirect.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         JD Auto Redirect
 // @namespace    http://twitter.com/ytzong
-// @version      0.7
+// @version      0.8
 // @description  JD Auto Redirect
 // @author       ytzong
-// @include      http://www.jd.com/*
-// @include      http://re.jd.com/*
-// @include      http://re.m.jd.com/*
-// @include      http://m.jd.com/sale/act/*
-// @include      http://item.m.jd.com/product/*
+// @include      http*://www.jd.com/*
+// @include      http*://re.jd.com/*
+// @include      http*://re.m.jd.com/*
+// @include      http*://m.jd.com/sale/act/*
+// @include      http*://item.m.jd.com/product/*
+// @include      http*://item.jd.com/*
 // @run-at       document-idle
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -25,4 +26,7 @@ if (domain == 'm.jd.com') {
 if (domain == 'item.m.jd.com' || domain == 're.jd.com' || domain == 're.m.jd.com') {
     var pathnames = location.pathname.split('/');
     location.href = 'http://item.jd.com/' + pathnames[pathnames.length - 1];
+}
+if (domain == 'item.jd.com') {
+    $('#choose-btns').append('<div class="btn"><a class="btn-append" href="http://gate.jd.com/InitCart.aspx?pid=' + window.location.pathname.replace('/', '').replace('.html', '') + '&pcount=1&ptype=1">加入购物车</a></div>');
 }
