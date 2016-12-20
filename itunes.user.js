8d3a2b72d0b0f53e8895cf39668f5aed7333a557
ytzong <ytzong7@gmail.com>
2016-12-20 13:56:41 +0800

    bug fixes

--- itunes.user.js
+++ itunes.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         iTunes
 // @namespace    http://twitter.com/ytzong
-// @version      0.1
+// @version      0.2
 // @author       ytzong
 // @include      https://itunes.apple.com/*
 // @grant        GM_addStyle
-// @run-at       document-start
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
 
 GM_addStyle('.truncate{height:auto !important}.price, .release-date, .in-app-purchases {background-color:yellow !important}');
-(function() {
-    'use strict';
 
-    // Your code here...
-})();
+var coverURL = $('div#left-stack div.artwork img').attr('src');
+console.log(coverURL);
+coverURL = coverURL.replace(/175x175/, "1024x1024");
+$('div#left-stack div.artwork img').closest('a').attr('href', coverURL);
