da034d24d322ff0d6636c3dbf4849d9d3a0d624b
ytzong <ytzong7@gmail.com>
2017-02-04 04:33:35 +0800

    bug fixes

--- itunes.user.js
+++ itunes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iTunes
 // @namespace    http://twitter.com/ytzong
-// @version      0.2
+// @version      0.3
 // @author       ytzong
 // @include      https://itunes.apple.com/*
 // @grant        GM_addStyle
@@ -10,7 +10,7 @@
 
 GM_addStyle('.truncate{height:auto !important}.price, .release-date, .in-app-purchases {background-color:yellow !important}');
 
-var coverURL = $('div#left-stack div.artwork img').attr('src');
+var coverURL = $('.product img').attr('src');
 console.log(coverURL);
 coverURL = coverURL.replace(/175x175/, "1024x1024");
-$('div#left-stack div.artwork img').closest('a').attr('href', coverURL);
+$('.product img').closest('a').attr('href', coverURL);
--- preload-for-zhihu.user.js
+++ preload-for-zhihu.user.js
@@ -1,16 +1,32 @@
 // ==UserScript==
 // @name         Preload for Zhihu
 // @namespace    http://twitter.com/ytzong
-// @version      0.1
+// @version      0.2
 // @description  Preload for Zhihu
 // @author       ytzong
 // @match        http://*.zhihu.com/*
 // @match        https://*.zhihu.com/*
 // @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js
 // ==/UserScript==
 
 
+
 function main() {
+	var rsslink = 'https://rss.lilydjwg.me/zhihuzhuanlan';
+	if (window.location.pathname.indexOf('following/columns') !== -1) {
+		$('.ContentItem-title').each(function(){
+			var link = rsslink + '/' + $(this).find('.ColumnLink').attr('href').split('/')[3];
+			if ($(this).find('.yt-rss').length === 0) $(this).append('(<a href="' + link + '" target="_blank" class ="yt-rss">RSS</a>)');
+		});
+	}
+	if (window.location.hostname == 'zhuanlan.zhihu.com') {
+		var title = $('.column-about .title');
+		if (title.find('.yt-rss').length === 0) {
+			var link = rsslink + title.attr('href');
+			title.append('(<a href="' + link + '" target="_blank" class ="yt-rss">RSS</a>)');
+		}
+	}
     $('img').each(function(){
         var dataSrc = $(this).attr('data-original');
         if (dataSrc && dataSrc != $(this).attr('src'))
