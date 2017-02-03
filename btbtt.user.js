b5b36dc28a1c3d9ad30a14d4726bde0e6229db81
ytzong <ytzong7@gmail.com>
2017-02-03 15:41:52 +0800

    bug fixes

---
+++ btbtt.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         BTBTT
+// @version      0.1
+// @author       ytzong
+// @description  BTBTT
+// @include    http://*.btbbt.*/*
+// @include    http://*.btbtt.*/*
+// @copyright    2017+
+// @run-at       document-start
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle('.width{width:auto !important}');
--- preload-images-for-wechat.user.js
+++ preload-images-for-wechat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Preload Images for WeChat
 // @namespace    http://twitter.com/ytzong
-// @version      0.7
+// @version      0.8
 // @description  Preload Images for WeChat
 // @author       ytzong
 // @match        http://mp.weixin.qq.com/*
@@ -9,7 +9,7 @@
 // @match        http://rd.wechat.com/*
 // @match        https://rd.wechat.com/*
 // @grant        GM_addStyle
-// @run-at       document-end
+// @run-at       document-start
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js
 // ==/UserScript==
 
@@ -17,15 +17,20 @@ Object.defineProperty(navigator, 'userAgent', {
     value: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50'
 });
 
-GM_addStyle('img{width:auto !important;height:auto !important}');
+GM_addStyle('@media screen and (min-width: 1024px) {.rich_media,.not_in_mm .qr_code_pc_inner {width:80% !important}}.rich_media_content {overflow:visible !important}.rich_media_content p, .rich_media_content p *{font-size:16px !important;line-leight:1.6 !important}.rich_media_content img{max-width:100% !important;width:auto !important;height:auto !important} .not_in_mm .qr_code_pc{position:fixed !important;top:auto !important;bottom:0 !important;right:0 !important;padding: 10px 0px !important;}');
 function main() {
     //$('title').text($('#activity-name:eq(0)').text());
+    
     $('img').each(function(){
         var dataSrc = $(this).attr('data-src');
-        if (dataSrc)
-            $(this).attr('src', dataSrc);
-            //.removeAttr('style').removeAttr('data-s').removeAttr('data-w').removeAttr('data-ratio');
+        var ext = '?';
+        if(typeof dataSrc !== "undefined")
+            if (dataSrc.indexOf('?') !== -1) ext = '&';
+        ext = ext + 'tp=png&wxfrom=5&wx_lazy=1';
+            $(this).attr('src', dataSrc ).removeAttr('style');
+            //.removeAttr('data-s').removeAttr('data-w').removeAttr('data-ratio');
     });
+    
     if (window.location.host == 'rd.wechat.com') {
         //gotoURL();
         window.location.href = $('#url').text();
