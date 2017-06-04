36874eba525050b9044c7e56cd889ac131f8d4d7
ytzong <ytzong7@gmail.com>
2017-06-04 23:36:10 +0800

    bug fixes

---
+++ just998.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Just998
+// @version      0.1
+// @author       ytzong
+// @description  Just998
+// @include      http://*just998.com/*
+// @copyright    2017+
+// @run-at       document-end
+// @grant        GM_addStyle
+// ==/UserScript==
+
+$('.content a').each(function(i){
+	var href = $(this).text();
+	if (~href.indexOf('...')) {
+	
+	} 
+	else {
+		if (~href.indexOf('http'))
+			$(this).attr('href', href).removeAttr('onclick');
+	}
+}); 
---
+++ smzdm.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         SMZDM
+// @version      0.1
+// @author       ytzong
+// @description  SMZDM
+// @include    http://*.btbbt.*/*
+// @include    http://*.btbtt.*/*
+// @copyright    2017+
+// @run-at       document-start
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle('body {background-image:none !important}.width{width:auto !important}');
