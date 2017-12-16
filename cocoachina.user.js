3f4cec594a68c99b1f8f36bf30bc553addbb10df
ytzong <ytzong7@gmail.com>
2017-12-16 14:31:28 +0800

    no message

--- cocoachina.user.js
+++ cocoachina.user.js
@@ -1,21 +1,17 @@
 // ==UserScript==
 // @name         CocoaChina
 // @namespace    https://twitter.com/ytzong
-// @version      0.4
+// @version      0.5
 // @author       ytzong
 // @include      http://www.cocoachina.com/bbs/*
 // @grant        GM_addStyle
-// @run-at       document-start
+// @run-at       document-end
 // ==/UserScript==
 
 GM_addStyle('#feedback,.tpc_content .fr,.tips,.r_one iframe,#ads_c_tpc,#newjob {display:none !important}# read_tpc, .tpc_content{max-width:650px;}');
 
-(function() {
-    'use strict';
-
-    // Your code here...
-    jQuery('td.tal a').each(function(){
-        if (jQuery(this).text() == '[广告投放]') jQuery(this).parent().parent().hide();
-        //console.log(jQuery(this).text());
-    });
-})();
+// Your code here...
+jQuery('td.tal a').each(function(){
+	if (jQuery(this).text() == '[广告投放]') jQuery(this).parent().parent().hide();
+	//console.log(jQuery(this).text());
+});
