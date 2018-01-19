78bd30117b5dea06e8f943b212d99c032e69fb20
ytzong <ytzong7@gmail.com>
2018-01-20 02:49:52 +0800

    no message

--- itunes.user.js
+++ itunes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iTunes
 // @namespace    https://twitter.com/ytzong
-// @version      0.6
+// @version      0.7
 // @author       ytzong
 // @include      https://itunes.apple.com/*
 // @grant        GM_addStyle
@@ -9,13 +9,29 @@
 // @description Show large icon
 // ==/UserScript==
 
-GM_addStyle('.truncate{height:auto !important}.price, .release-date, .in-app-purchases {background-color:yellow !important}.view-in-itunes{display:inline-block !important}');
+GM_addStyle('.inline-list__item--bulleted{background-color:yellow !important}');
 
-var coverURL = $('.product img').attr('src');
-console.log(coverURL);
-coverURL = coverURL.replace(/175x175/, "1024x1024");
-coverURL = coverURL.replace(/.jpg/, ".png");
-$('.product img').closest('a').attr('href', coverURL).attr('target', '_blank');
+window.setTimeout(main, 3000);
+function main(){
+	var appLink = location.href;
+	appLink = appLink.replace('http://', 'https://');
+	appLink = appLink.replace('https://', 'itms://');
+	var button = '<div class="product-header__routes"><p class="product-header__routes__cta"><a href="'+ appLink +'" aria-label="View in iTunes" class="we-button we-button--outlined we-button--external ember-view">View in <span class="we-button__app-text">iTunes</span></a></p></div>';
+	if ($('.product-header__routes').length == 0) $('.product-header').append(button);
 
-var titleEle = $('#title h1');
-titleEle.wrap('<a href="https://www.appannie.com/search/?q=' + encodeURIComponent(titleEle.text()) + '" target="_blank"/>');
+	var time = $('.version-history__item__release-date').eq(0).text();
+	$('.product-header').append('<span style="background:yellow;color:#8e8e93">' + time + '</span>');
+
+	var coverURL = $('.product-hero__artwork img').attr('src');
+	console.log(coverURL);
+	coverURL = coverURL.replace(/246x0w/, "1024x0w");
+	coverURL = coverURL.replace(/.jpg/, ".png");
+	$('.product-hero__artwork').wrap('<a href="' + coverURL + '" target="_blank"></a>');
+
+	var badge = $('.badge--product-title');
+	badge.remove();
+	var title = $('.product-header__title').text();
+	title = $.trim(title);
+	$('.product-header__title').append(badge);
+	$('.product-header__title').wrap('<a href="https://www.appannie.com/search/?q=' + encodeURIComponent(title) + '" target="_blank"></a>');
+}
