937570a16314aedadf48cd80d2d518ffcae6401e
ytzong <ytzong7@gmail.com>
2018-03-15 12:46:45 +0800

    no message

---
+++ zimuku.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         zimuku
+// @version      0.1
+// @author       ytzong
+// @description  zimuku
+// @include      http://www.zimuku.*/*
+// @copyright    2018+
+// @run-at       document-end
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle('.modal-open{overflow:auto!important}');
