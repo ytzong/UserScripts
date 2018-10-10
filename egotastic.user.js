a4dd41908495b6b9ee590c47f8e6beb45bbf75b0
ytzong <ytzong7@gmail.com>
2018-10-10 14:24:02 +0800

    no message

--- egotastic.user.js
+++ egotastic.user.js
@@ -1,15 +1,25 @@
 // ==UserScript==
-// @name         Egotastic All Stars
-// @version      0.1
+// @name         Egotastic
+// @version      0.2
 // @author       ytzong
-// @description  Egotastic All Stars
-// @include      http://*egoallstars.com/*
-// @include      https://*egoallstars.com/*
+// @description  Egotastic
+// @include      http*://*egoallstars.com/*
+// @include      http*://*egotastic.com/*
 // @copyright    2018+
 // @run-at       document-end
 // @grant        GM_addStyle
 // ==/UserScript==
-GM_addStyle('@media (min-width: 992px){.col-md-8, .col-md-4, .container, .e-box-comments{width:100%!important;}.e-box{width:33.3%!important;}.e-big-pic{margin-bottom:0 !important}}');
+
+if ($('.e-headline').text().trim() == 'PAGE NOT FOUND') {
+    setTimeout(location.reload(), 2000);
+}
+
+//index
+GM_addStyle('.single-post-lg{display:inline-block;width:33%;vertical-align: top;}.malinky-ajax-pagination-loading{text-align:center}.pagination{display:block !important;padding-bottom:100px;}#cookie-law-info-bar, .h-fixed-footer, #cookie-law-info-again{display:none}.h-maincontent .h-whitebg h1{margin-bottom:15px !important; font-size: 24px !important;}.h-maincontent .h-whitebg{padding:15px !important}');
+$('.h-sidecontent').insertBefore('.h-maincontent');
+
+//detail 
+GM_addStyle('@media (min-width: 992px){.col-md-8, .col-md-4, .container, .e-box-comments, .h-box-comments{width:100%!important;}.e-box{width:33.3%!important;}.e-big-pic{margin-bottom:0 !important}}');
 GM_addStyle('.e-circle-image, #e-comments, .p-comments{display:none}#yt-img{padding:100px 0 300px;margin:0 -30px;background-color:black;text-align:center;}#yt-img img{display:block;margin:15px auto;/*width:100%;*/max-width:100%}#yt-close{display:none;margin:0 -30px -50px;padding:70px;background-color:yellow;color:yellow;text-align:center;}');
 
 $('.e-right').insertBefore('.e-left');
@@ -26,6 +36,8 @@ function getNext(url) {
             $('#yt-img').append(img);
             next = $(html).find('#nextLink').attr('href');
             setTimeout(getNext(next), 1000);
+        }).fail(function() {
+            setTimeout(getNext(url), 2000);
         });
     }
 }
