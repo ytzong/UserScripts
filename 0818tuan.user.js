6f599a4c72c7ac2189e699f7b07754e8ad740cd1
ytzong <ytzong7@gmail.com>
2018-10-12 15:54:59 +0800

    no message

--- 0818tuan.user.js
+++ 0818tuan.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         0818tuan
-// @version      0.7
+// @version      0.8
 // @author       ytzong
 // @description  0818tuan
 // @include      http://*0818tuan.com/*
@@ -11,10 +11,29 @@
 // @require      https://cdn.rawgit.com/jprichardson/string.js/master/dist/string.min.js
 // ==/UserScript==
 
-GM_addStyle('');
-if (location.pathname.includes('/xb/')) {
+GM_addStyle('.yt-source{margin-top:3em;text-align:center}');
+if (location.pathname.includes('/xbhd/')) {
 
 }
+$('.list-group a').each(function(i){
+	let title = $(this).text();
+	let blacks = ['促销活动', '必看活动', '微信专享', '：'];
+	let shoudHide = 0;
+	if ($(this).find('img').length) shoudHide = 1;
+	for (let b of blacks) {
+		if (S(title).contains(b)) shoudHide = 1;
+	}
+	if (shoudHide == 1) $(this).hide();
+});
+$('.post-content p').each(function(i){
+	let title = $(this).text();
+	let blacks = ['上一篇：', '下一篇：'];
+	let shoudHide = 0;
+	for (let b of blacks) {
+		if (S(title).contains(b)) shoudHide = 1;
+	}
+	if (shoudHide == 1) $(this).hide();
+});
 $('.post-content a').each(function(i){
 	var text = $(this).text();
 	if (text.includes('http') && !text.includes('...')) {
@@ -28,20 +47,13 @@ $('.post-content a').each(function(i){
             .s;
 	url = decodeURIComponent(url);
 	$(this).attr('href', url);
-
-	var sourceURL = $('.post-content .text-center a').eq(0);
-	sourceURL.text(sourceURL.attr('href'));
-	sourceURL.parent().html(sourceURL.html()).next().remove();
 });
 
-$('.list-group a').each(function(i){
-	let title = $(this).text();
-	let blacks = ['促销活动', '必看活动', '微信专享', '：'];
-	let shoudHide = 0;
-	if ($(this).find('img').length) shoudHide = 1;
-	for (let b of blacks) {
-		if (S(title).contains(b)) shoudHide = 1;
-	}
-	if (shoudHide == 1) $(this).hide();
-});
+var sourceURL = $('.post-content .text-center a').eq(0);
+var sourceHTML = sourceURL.attr('href');
+if (S(sourceHTML).startsWith('http')) {
+    sourceHTML = '<p class="yt-source">来源：<a href="' + sourceHTML + '">' + sourceHTML + '</a></p>';
+    $('.post-content').append(sourceHTML);
+}
+sourceURL.parent().hide();
 
--- egotastic.user.js
+++ egotastic.user.js
@@ -13,22 +13,31 @@
 if ($('.e-headline').text().trim() == 'PAGE NOT FOUND') {
     setTimeout(location.reload(), 2000);
 }
-
+GM_addStyle('#yt-close{display:block;margin:0 -50px -100px;padding:50vh 0;background-color:black;color:white;text-align:center;}');
+$('body').append('<a href="javascript:window.close()" id="yt-close" title="Close This Tab">Close This Tab</a>');
 //index
 GM_addStyle('.single-post-lg{display:inline-block;width:33%;vertical-align: top;}.malinky-ajax-pagination-loading{text-align:center}.pagination{display:block !important;padding-bottom:100px;}#cookie-law-info-bar, .h-fixed-footer, #cookie-law-info-again{display:none}.h-maincontent .h-whitebg h1{margin-bottom:15px !important; font-size: 24px !important;}.h-maincontent .h-whitebg{padding:15px !important}');
 $('.h-sidecontent').insertBefore('.h-maincontent');
 
 //detail 
 GM_addStyle('@media (min-width: 992px){.col-md-8, .col-md-4, .container, .e-box-comments, .h-box-comments{width:100%!important;}.e-box{width:33.3%!important;}.e-big-pic{margin-bottom:0 !important}}');
-GM_addStyle('.e-circle-image, #e-comments, .p-comments{display:none}#yt-img{padding:100px 0 300px;margin:0 -30px;background-color:black;text-align:center;}#yt-img img{display:block;margin:15px auto;/*width:100%;*/max-width:100%}#yt-close{display:none;margin:0 -30px -50px;padding:70px;background-color:yellow;color:yellow;text-align:center;}');
+GM_addStyle('.e-circle-image, #e-comments, .p-comments,.e-image-wrapper.whitespace, .h-adholder,.p-wraper-box,div[data-mh="p-group-posts"]{display:none}#yt-img{padding:150px 0 30px;margin:0 -30px -1px;background-color:black;text-align:center;}#yt-img img{display:block;margin:30px auto;/*width:100%;*/max-width:100%}');
 
 $('.e-right').insertBefore('.e-left');
-$('.e-big-pic').append('<div id="yt-img"></div><a href="javascript:window.close()" id="yt-close">Close</a>');
+$('.e-big-pic').append('<div id="yt-img"></div>');
+$('.g-slider').after('<div id="yt-img"></div>');
 
 var next = $('.e-circle-image a').attr('href');
+if ($('.g-slider').length > 0) {
+    $('.g-slider').hide();
+    var img = $('.g-fancybox');
+    $('#yt-img').append(img);
+    next = $('#nextLink').attr('href');
+}
 getNext(next);
 
 function getNext(url) {
+    console.log(url);
     if (url.length > 0) {
         $.get(url, function(result) {
             var html = $.parseHTML( result );
