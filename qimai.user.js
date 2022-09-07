11e5ca741db61bcc9ebe30875c6ea24aec87446a
ytzong <ytzong7@gmail.com>
2022-09-07 23:36:57 +0800

    no message

--- qimai.user.js
+++ qimai.user.js
@@ -16,7 +16,9 @@ if (domain.includes('qimai')) {
     GM_addStyle(`
   #footer-guide, .tool-side, a[href="http://u.qimai.cn/3xk"]{display:none!important}
   #rank-top-list .data-table .app-info-wrap .app-info .app-name{max-width:none!important}
-  .data-table tr td .app-info-wrap .icon>img{width:60px!important;height:60px!important}
+.data-table tr td .app-info-wrap .icon{position:relative}
+.data-table tr td .app-info-wrap .icon>img{width:100px!important;height:100px!important;border:0 none!important;border-radius:0 !important}
+.data-table tr td .app-info-wrap .icon:after{content: "";width:100px;height:100px;background-image: url("https://rss.ytzong.com/css/img/mask100_2x.png");background-size: cover;position: absolute;top: 0;left: 0;}
   .app-info .basic-info .app-screenshot .screenshot-list .screenshot-box img{max-height:500px!important;height:auto!important}
   `);
 
@@ -47,6 +49,14 @@ if (domain.includes('sensortower')) {
             if (!$(this).text().includes('< $5k')) {
                 $(this).parents('tr').addClass('yt-highlight')
             }
+        })
+        $('.app-icon a').each(function () {
+            $(this).attr('target', '_blank')
+        })
+        $('.app-info .name').each(function () {
+            let appID = $(this).attr('href').split('/').slice(-2)[0]
+            let appURL = 'https://apps.apple.com/us/app/id' + appID
+            $(this).attr('href', appURL).attr('target', '_blank')
         })
     }, 2000)
 
