e782e8a7c1794c8fae1b38a1c1760cc4c46dab2b
ytzong <ytzong7@gmail.com>
2022-09-07 17:02:30 +0800

    no message

--- qimai.user.js
+++ qimai.user.js
@@ -1,32 +1,53 @@
 // ==UserScript==
 // @name         QiMai
-// @version      2022.09.06
+// @version      2022.09.07
 // @author       ytzong
 // @description  QiMai
 // @include      http*://*qimai.*/*
+// @include      http*://app.sensortower.com/ios/publisher/publisher/*
 // @copyright    ytzong
 // @run-at       document-end
 // @grant        GM_addStyle
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
 
-GM_addStyle(`
-#footer-guide, .tool-side, a[href="http://u.qimai.cn/3xk"]{display:none!important}
-#rank-top-list .data-table .app-info-wrap .app-info .app-name{max-width:none!important}
-.data-table tr td .app-info-wrap .icon>img{width:60px!important;height:60px!important}
-`);
+let domain = location.host
+if (domain.includes('qimai')) {
+    GM_addStyle(`
+  #footer-guide, .tool-side, a[href="http://u.qimai.cn/3xk"]{display:none!important}
+  #rank-top-list .data-table .app-info-wrap .app-info .app-name{max-width:none!important}
+  .data-table tr td .app-info-wrap .icon>img{width:60px!important;height:60px!important}
+  .app-info .basic-info .app-screenshot .screenshot-list .screenshot-box img{max-height:500px!important;height:auto!important}
+  `);
 
-window.setTimeout(function () {
+    window.setTimeout(function () {
 
-    $('.ivu-breadcrumb-item-link a').each(function () {
-        let publisherID = $(this).attr('href')
-        console.log(publisherID)
-        if (publisherID.includes('/publisher/')) {
-            publisherID = publisherID.split('/')[4]
+        $('.ivu-breadcrumb-item-link a').each(function () {
+            let publisherID = $(this).attr('href')
             console.log(publisherID)
-            $(this).attr('href', 'https://app.sensortower.com/ios/publisher/publisher/' + publisherID)
-            $(this).text($(this).text() + ' »')
-        }
-    })
+            if (publisherID.includes('/publisher/')) {
+                publisherID = publisherID.split('/')[4]
+                console.log(publisherID)
+                let sensortower = 'https://app.sensortower.com/ios/publisher/publisher/' + publisherID
+                $(this).attr('href', sensortower)
+                $(this).text($(this).text() + ' »')
+                location.href = sensortower
+            }
+        })
 
-}, 2000)
+    }, 2000)
+}
+if (domain.includes('sensortower')) {
+    GM_addStyle(`
+    .summary-item:last-child a,
+    .yt-highlight{background-color:yellow!important}
+  `);
+    window.setTimeout(function () {
+        $('td.app-metric:last-child a').each(function () {
+            if (!$(this).text().includes('< $5k')) {
+                $(this).parents('tr').addClass('yt-highlight')
+            }
+        })
+    }, 2000)
+
+}
