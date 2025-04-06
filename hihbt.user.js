2be3fc1459e03c6067f3456161bc0d372375927d
ytzong <ytzong7@gmail.com>
2025-04-06 16:23:15 +0800

    no message

--- hihbt.user.js
+++ hihbt.user.js
@@ -4,12 +4,13 @@
 // @description HiHBT
 // @include     http*://*hihbt.*/*
 // @include     http*://*51svp.*/*
-// @include     http*://*51btsite.*/*
+// @include     http*://*btsite.*/*
+// @include     http*://*btsite.*/*
 // @include     http*://*2x5t.*/*
-// @version     2022.04.14
+// @version     2025.04.06
 // @grant       GM_addStyle
 // @run-at      document-end
-// @require     https://code.jquery.com/jquery-3.6.0.slim.min.js 
+// @require     https://code.jquery.com/jquery-3.6.0.slim.min.js
 // @require     https://cdn.rawgit.com/jprichardson/string.js/master/dist/string.min.js
 // ==/UserScript==
 
@@ -22,7 +23,7 @@ GM_addStyle('#sidebar, .s-weixin, .nav-single, .post-navigation, .single-cat-tag
 if (pathname.includes('18APPTZ') || domain.includes('365ksp')) {
     window.close()
 }
-if (domain.includes('51bt')) {
+if (domain.includes('btsite')) {
     GM_addStyle('h3, img{display:none !important}')
     $('.caption-full p').eq(-1).hide()
 }
